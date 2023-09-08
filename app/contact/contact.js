eb3d1f6c90a46a2a05cf3e3c77d052b465dc5247
Rajesh <rajesh11022002@gmail.com>
2023-09-08 18:08:42 +0530

    added module.css

---
+++ app/contact/contact.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from '../contact/contact.module.css';
+const contact = () => {
+    return <div className={styles.div_contact}>contact</div>;
+};
+
+export default contact;
